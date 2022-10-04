d62a9bb412b6ea523982dc4889fcb5cf8f874fdb
howard <howardchiu01@gmail.com>
2022-10-05 00:33:20 +0800

    刪除做好了

---
+++ src/main/java/com/example/hr_system/bean/login/LoginDelteDto.java
@@ -0,0 +1,14 @@
+package com.example.hr_system.bean.login;
+
+import lombok.Data;
+
+@Data
+public class LoginDelteDto {
+
+    private  int    loginId;
+    private  String loginAccount;
+    private  String password;
+    private  int    peopleId;
+
+
+}
